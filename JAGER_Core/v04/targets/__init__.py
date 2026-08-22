d3256fca43a40b77cc7213b34cbc1fd96d3c778b
kumaran <k31311558@gmail.com>
2026-08-23 01:13:30 +0530

    Create __init__.py

---
+++ JAGER_Core/v04/targets/__init__.py
@@ -0,0 +1,19 @@
+from .base import (
+    BaseTarget,
+    TargetResult,
+)
+
+from .linux_target import (
+    LinuxTarget,
+)
+
+from .ai_sandbox_target import (
+    AISandboxTarget,
+)
+
+__all__ = [
+    "BaseTarget",
+    "TargetResult",
+    "LinuxTarget",
+    "AISandboxTarget",
+]
