a0a953fa4fab3c2e749ff81d759f71b812603377
kumaran <k31311558@gmail.com>
2026-08-23 13:17:57 +0530

    Create event_types.py

---
+++ JAGER_Core/v04/core/event_types.py
@@ -0,0 +1,47 @@
+EXPERIMENT_CREATED = (
+    "experiment.created"
+)
+
+EXPERIMENT_STARTED = (
+    "experiment.started"
+)
+
+EXPERIMENT_COMPLETED = (
+    "experiment.completed"
+)
+
+EXPERIMENT_FAILED = (
+    "experiment.failed"
+)
+
+ITERATION_STARTED = (
+    "iteration.started"
+)
+
+ITERATION_COMPLETED = (
+    "iteration.completed"
+)
+
+EXECUTION_STARTED = (
+    "execution.started"
+)
+
+EXECUTION_COMPLETED = (
+    "execution.completed"
+)
+
+EXECUTION_FAILED = (
+    "execution.failed"
+)
+
+RUNTIME_STARTED = (
+    "runtime.started"
+)
+
+RUNTIME_STOPPED = (
+    "runtime.stopped"
+)
+
+RUNTIME_ERROR = (
+    "runtime.error"
+)
